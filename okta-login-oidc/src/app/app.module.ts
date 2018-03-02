fb7296c24aeb73da3ccd70a80fe41e533a041f2f
Tapio Mattila <tapsa87@gmail.com>
2018-03-02 15:50:02 +0200

    Added project folder structure

--- okta-login-oidc/src/app/app.module.ts
+++ okta-login-oidc/src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessagesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
