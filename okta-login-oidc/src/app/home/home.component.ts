116d0e83c472cc1813e1134faec1c971bf509d04
Tapio Mattila <tapsa87@gmail.com>
2018-03-02 16:24:45 +0200

    Added a full project example frontend

--- okta-login-oidc/src/app/home/home.component.ts
+++ okta-login-oidc/src/app/home/home.component.ts
@@ -1,4 +1,23 @@
+
+/*!
+ * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
+interface ResourceServerExample {
+  label: String;
+  url: String;
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +25,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  idToken: Object;
+  resourceServerExamples: Array<ResourceServerExample>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public oktaAuth: OktaAuthService) {
+    const accessToken = this.oktaAuth.getAccessToken();
+    this.idToken = this.oktaAuth.getIdToken();
+    this.resourceServerExamples = [
+      {
+        label: 'Node/Express Resource Server Example',
+        url: 'https://github.com/okta/samples-nodejs-express-4/tree/master/resource-server',
+      },
+      {
+        label: 'Java/Spring MVC Resource Server Example',
+        url: 'https://github.com/okta/samples-java-spring-mvc/tree/master/resource-server',
+      },
+    ]
   }
 
+  ngOnInit() {
+
+  }
 }
--- okta-login-oidc/src/app/profile/profile.component.ts
+++ okta-login-oidc/src/app/profile/profile.component.ts
@@ -1,4 +1,23 @@
+
+/*!
+ * Copyright (c) 2018, Okta, Inc. and/or its affiliates. All rights reserved.
+ * The Okta software accompanied by this notice is provided pursuant to the Apache License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+
+interface Claim {
+  claim: String,
+  value: String
+}
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +25,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  idToken;
+  claims: Array<Claim>
 
-  constructor() { }
+  constructor(public oktaAuth: OktaAuthService) {
+    this.idToken = this.oktaAuth.getIdToken();
+    this.claims = Object.entries(this.idToken.claims).map(entry => ({ claim: entry[0], value: entry[1] }));
+  }
 
   ngOnInit() {
   }
 
 }
+
